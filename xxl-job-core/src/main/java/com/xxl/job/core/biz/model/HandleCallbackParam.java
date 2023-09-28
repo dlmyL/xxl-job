0367c9ea9b7d3209ff11ca464c10dfe914a113d6
dlmyL <916748598@qq.com>
2023-09-28 23:51:25 +0800

    docs: 添加XXL-JOB源码注释

--- xxl-job-core/src/main/java/com/xxl/job/core/biz/model/HandleCallbackParam.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/biz/model/HandleCallbackParam.java
@@ -1,5 +1,6 @@
 package com.xxl.job.core.biz.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.io.Serializable;
  * <h1>这个就是用于执行器回调定时任务执行结果的包装类，执行结果的信息用这个类的对象封装</h1>
  */
 @Data
+@AllArgsConstructor
 public class HandleCallbackParam implements Serializable {
 
     private static final long serialVersionUID = 42L;
