07902b92aa9567b81a24cf0c638219a3254b7eda
dlmyL <916748598@qq.com>
2023-09-28 23:52:22 +0800

    docs: 添加XXL-JOB源码注释

--- xxl-job-core/src/main/java/com/xxl/job/core/biz/model/IdleBeatParam.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/biz/model/IdleBeatParam.java
@@ -1,5 +1,6 @@
 package com.xxl.job.core.biz.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.io.Serializable;
  * @author xuxueli 2020-04-11 22:27
  */
 @Data
+@AllArgsConstructor
 public class IdleBeatParam implements Serializable {
 
     private static final long serialVersionUID = 42L;
